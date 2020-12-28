46d8c857d04efc9b4ffe9703b27c2aa2821d5e8c
daisuke-c <09p061@gmail.com>
2020-12-28 18:21:18 +0900

    フォームの入力内容がボタンを押したあと初期化される仕様に変更、タグの名前を一部変更

--- kadai1/js/main.js
+++ kadai1/js/main.js
@@ -4,6 +4,7 @@
 const textNode = document.getElementById('text');
 const btn = document.getElementById('btn');
 const table = document.getElementById('table');
+const formNode = document.getElementById('form');
 //カウント用の変数を宣言
 let count = 0
 
@@ -14,6 +15,8 @@ btn.addEventListener('click', function (e) {
 
   //Nodeからテキストの値を取得
   const text = textNode.value;
+  //formタグからラジオボタンの要素を取得(後ほどの課題で使用可能性有)
+  //const formRadioNode = formNode.check;
 
   //値が入ってるかどうか
   if (text) {
@@ -23,8 +26,8 @@ btn.addEventListener('click', function (e) {
     const tr = document.createElement('tr');
     const tdID = document.createElement('td');
     const tdComment = document.createElement('td');
-    const tdFinished = document.createElement('td');
-    const tdFinished2 = document.createElement('td');
+    const tdBtnWorks = document.createElement('td');
+    const tdBtnDelete = document.createElement('td');
 
     //ボタン用のテキストを作成
     const status1 = '作業中';
@@ -33,10 +36,10 @@ btn.addEventListener('click', function (e) {
     //各タグに必要なテキストまたはクラス名を追加
     tdID.textContent = count;
     tdComment.textContent = text;
-    tdFinished.textContent = status1;
-    tdFinished2.textContent = status2;
-    tdFinished.classList.add('btn');
-    tdFinished2.classList.add('btn');
+    tdBtnWorks.textContent = status1;
+    tdBtnDelete.textContent = status2;
+    tdBtnWorks.classList.add('btn');
+    tdBtnDelete.classList.add('btn');
 
     //trの各子要素を追加する関数
     function addTd(td) {
@@ -45,12 +48,15 @@ btn.addEventListener('click', function (e) {
 
     addTd(tdID);
     addTd(tdComment);
-    addTd(tdFinished);
-    addTd(tdFinished2);
-
+    addTd(tdBtnWorks);
+    addTd(tdBtnDelete);
 
     //tableタグ内,trの末尾に宣言したtr要素を追加
     table.insertBefore(tr, null);
+
+    //フォームに入力した内容をリセット
+    formNode.reset();
+
   }
 })
 
