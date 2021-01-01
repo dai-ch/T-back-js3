20e5efcee775f8b914458247fd43998d9e954dd9
daisuke-c <09p061@gmail.com>
2021-01-02 03:40:11 +0900

    課題②を実施（レビュー前）

--- kadai2/main.js
+++ kadai2/main.js
@@ -10,120 +10,83 @@ const radioBtn = document.getElementsByName('check');
 
 //Todoリストを管理する配列を宣言
 const todoList = [];
-//console.log(todoList.length);
+
 //IDとtodiListで使用する変数を定義
 let num = 0;
-
-
 //追加ボタンを押した時の処理
 addBtn.addEventListener('click', function (e) {
   //ページ遷移を注視するためのイベント
   e.preventDefault();
-
   //入力された値が空なら処理を中断
   if (!inputText.value) {
     return;
   }
-
   //todoListに格納するオブジェクトの宣言（trタグ内のtdタグで表示）
   const todo = {
     id: todoList.length,
     comment: inputText.value,
     status: '作業中',
-    delete: '削除',
   }
-
+  //配列の末尾にtodoを追加
   todoList.push(todo);
-
-  //trタグとtdタグをそれぞれ作成
-  // const tr = document.createElement('tr');
-  const tdID = document.createElement('td');
-  const tdComment = document.createElement('td');
-  const tdBtnWorks = document.createElement('td');
-  const tdBtnDelete = document.createElement('td');
-
-  // //idのインデックス番号取得
-  todo.id = todoList.length;
-  //Nodeからテキストの値を取得
-  todo.comment = inputText.value;
-
   //フォームに入力した内容をリセット
   formNode.reset();
+  //テキストを追加・表示する関数を実行
+  showText(todoList);
+})
 
-  //テキストを表示する関数
-  function displayText(textItem) {
-
+//テキストを追加・表示する関数
+const showText = function (todoList) {
+  //(2回目以降の追加ボタン押下時に作動)
+  //HTMLに表示されているリストを全て削除する
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+  //配列から要素数分取得
+  todoList.forEach(function (todo, i) {
+    //trタグとtdタグをそれぞれ作成
     const tr = document.createElement('tr');
+    const tdId = document.createElement('td');
+    const tdComment = document.createElement('td');
+    const tdBtnWorks = document.createElement('td');
     //各タグに必要なテキストまたはクラス名を追加
-    tdID.textContent = textItem['id'];
-    tdComment.textContent = textItem['comment'];
-    tdBtnWorks.textContent = textItem['status'];
-    tdBtnDelete.textContent = textItem['delete'];
+    tdId.textContent = i + 1;
+    tdComment.textContent = todo['comment'];
+    tdBtnWorks.textContent = todo['status'];
     tdBtnWorks.classList.add('btn');
-    tdBtnDelete.classList.add('btn', 'deleteBtn');
 
     //trの各子要素を追加する関数
-    function addTd(td) {
-      tr.appendChild(td);
-    }
-    addTd(tdID);
-    addTd(tdComment);
-    addTd(tdBtnWorks);
-    addTd(tdBtnDelete);
+    tr.appendChild(tdId);
+    tr.appendChild(tdComment);
+    tr.appendChild(tdBtnWorks);
+    //iはtodolistの配列の要素番号
+    tr.appendChild(deleteBtn(i));
 
     //tableタグ内,trの末尾に宣言したtr要素を追加
     tableBody.insertBefore(tr, null);
-  }
+  });
+}
 
-  displayText(todoList[todoList.length - 1]);
-
+//削除ボタンを作成＋表示する関数
+const deleteBtn = function (index) {
+  const tdBtnDelete = document.createElement('td');
+  tdBtnDelete.textContent = '削除';
+  tdBtnDelete.classList.add('btn');
+  tdBtnDelete.setAttribute('id', 'deleteBtn');
   //削除ボタンを押した時の処理
   tdBtnDelete.addEventListener('click', function () {
-
     //削除予定のtodoListのインデックス番号を取得
-    const deleteListNum = todoList.findIndex(index => {
-      return index.id === todo.id;
+    const deleteListNum = todoList.findIndex(todo => {
+      return todo.id === index;
     });
-
-    //tableBodyの子要素を削除
-    const tableBody = tdBtnDelete.parentNode.parentNode;
-
-    //一度、HTMLに表示されているリストを全て削除する
-    while (tableBody.firstChild) {
-      tableBody.removeChild(tableBody.firstChild);
-    }
-
     //クリックされたボタンに対応するNodeListのプロパティを削除
     todoList.splice(deleteListNum, 1);
-
     //todoの各IDを再度連番になりように振り直す
     for (let i = 0; i < todoList.length; i++) {
-      todoList[i]['id'] = i + 1;
+      todoList[i]['id'] = i;
     }
-    console.log(todoList);
-    console.log(tableBody);
-    console.log(tdBtnDelete.parentNode);
-
-
-    todoList.forEach(list => {
-      console.log(list);
-      displayText(list);
-    });
-
-    // for (let i = 0; i < todoList.length; i++) {
-    //   displayText(todoList[i]);
-    //   console.log(todoList[i]);
-    // }
+    //todoに格納されている値をテキストに追加＋表示
+    showText(todoList);
   })
-
-
-
-
-})
-
-
-
-
-
-
-
+  return tdBtnDelete;
+}
